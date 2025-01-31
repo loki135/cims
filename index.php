f80fe92d7ec7602d77ea5a47f1303c191b0393bb
lokesh <93985411+loki135@users.noreply.github.com>
2025-01-31 08:07:48 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,36 @@
+
+	<?php include('common/header.php') ?>
+	<div class="home-div"></div>
+		<div class="w-100 in-ad-ap">
+			<div class="row m-auto text-center">
+				<div class="col-md-4">
+					<div class="card">
+						<div class="card-body">
+							<h5 class="card-title">Admin</h5>
+							<p class="card-text">Admin can manage the whole system. Admin can add, delete, update the records of teachers, students, courses, subjects, time table, class results,student attendance.</p>
+							<a href="Login/login.php" class="btn btn-primary">Login</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="card">
+						<div class="card-body">
+							<h5 class="card-title">Teacher</h5>
+							<p class="card-text">Teacher can manage the records of students, courses, subjects, time table, class results,student attendance.</p>
+							<a href="Login/login.php" class="btn btn-primary">Login</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="card">
+						<div class="card-body">
+							<h5 class="card-title">Student</h5>
+							<p class="card-text">Student can view the records of courses, subjects, time table, class results.</p>
+							<a href="Login/login.php" class="btn btn-primary">Login</a>
+						</div>
+					</div>
+				</div>
+		</div>
+</body>
+</html>
+
